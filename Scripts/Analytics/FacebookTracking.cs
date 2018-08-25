c79026f6fe872c9ee4419dcfd50d9ecc381dc4db
Aaron Sarazan <aaron@sarazan.net>
2018-08-25 14:58:15 -0700

    analytics

---
+++ Scripts/Analytics/FacebookTracking.cs
@@ -0,0 +1,24 @@
+﻿#if STENCIL_FACEBOOK
+
+using System.Collections.Generic;
+using Facebook.Unity;
+
+namespace Analytics
+{
+    public class FacebookTracking : ITracker
+    {
+        public ITracker Track(string name, Dictionary<string, object> eventData)
+        {
+            FB.LogAppEvent(name, null, eventData);
+            return this;
+        }
+
+        public ITracker SetUserProperty(string name, object value)
+        {
+            // Nothing.
+            return this;
+        }
+    }
+}
+
+#endif
