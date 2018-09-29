411ba8399eae5964aa760897a410c5f0708e5370
Aaron Sarazan <aaron@sarazan.net>
2018-09-28 22:17:27 -0700

    remote fix

--- Scripts/RemoteConfig/StencilRemote.cs
+++ Scripts/RemoteConfig/StencilRemote.cs
@@ -18,7 +18,8 @@ namespace Scripts.RemoteConfig
         
         public static long GetProdVersion()
         {
-            return GetValue("version", true)?.LongValue(long.MaxValue) ?? long.MaxValue;
+            if (!GameInit.FirebaseReady) return long.MaxValue;
+            return FirebaseRemoteConfig.GetValue("version").LongValue(long.MaxValue);
         }
         
         public static bool IsProd()
@@ -28,13 +29,13 @@ namespace Scripts.RemoteConfig
             return localVersion <= GetProdVersion();
         }
 
-        public static ConfigValue? GetValue(string key, bool forceProd = false)
+        public static ConfigValue GetValue(string key, bool forceProd = false)
         {
             var orig = key;
             if (!forceProd) key = key.Process();
-            if (!GameInit.FirebaseReady) return null;
+            if (!GameInit.FirebaseReady) return new ConfigValue();
             var value = FirebaseRemoteConfig.GetValue(key);
-            if (!IsProd() && !value.HasValue())
+            if (!value.HasValue() && !IsProd())
                 value = FirebaseRemoteConfig.GetValue(orig);
             return value;
         }
@@ -47,7 +48,42 @@ namespace Scripts.RemoteConfig
         
         public static bool HasValue(string key)
         {
-            return GetValue(key.Process())?.HasValue() ?? false;
+            return GetValue(key.Process()).HasValue();
+        }
+        
+        public static long LongValue(string key, long defaultValue = default(long))
+        {
+            return GetValue(key.Process()).LongValue(defaultValue);
+        }
+                 
+        public static int IntValue(string key, int defaultValue = default(int))
+        {
+            return GetValue(key.Process()).IntValue(defaultValue);
+        }
+         
+        public static string StringValue(string key, string defaultValue = default(string))
+        {
+            return GetValue(key.Process()).StringValue;
+        }
+                 
+        public static double DoubleValue(string key, double defaultValue = default(double))
+        {
+            return GetValue(key.Process()).DoubleValue(defaultValue);
+        }
+                 
+        public static float FloatValue(string key, float defaultValue = default(float))
+        {
+            return GetValue(key.Process()).FloatValue(defaultValue);
+        }
+                 
+        public static bool BoolValue(string key, bool defaultValue = default(bool))
+        {
+            return GetValue(key.Process()).BoolValue(defaultValue);
+        }
+                 
+        public static IEnumerable<byte> ByteArrayValue(string key, IEnumerable<byte> defaultValue = null)
+        {
+            return GetValue(key.Process()).ByteArrayValue(defaultValue);
         }
     }
 }
