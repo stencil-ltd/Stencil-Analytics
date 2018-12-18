70ec136db9eaa5e20fd4206160479e6c3a7018e6
Aaron Sarazan <aaron@sarazan.net>
2018-12-18 10:33:59 -0800

    update tenjin support

---
+++ Scripts/Tenjin/StencilTenjin.cs
@@ -0,0 +1,49 @@
+using Binding;
+using Game.Screens.Gems;
+using Plugins.UI;
+using Scripts.RemoteConfig;
+using UI;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+namespace Scripts.Tenjin
+{
+    [RequireComponent(typeof(TenjinIapHelper))]
+    public class StencilTenjin : Permanent<StencilTenjin>
+    {
+        public string apiKey;
+        public BaseTenjin tenjin { get; private set; }
+        
+        [RemoteField("tenjin_base")] 
+        public bool baseEnabled = true;
+
+        [RemoteField("tenjin_iap")]
+        public bool iapEnabled = true;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            if (!Valid) return;
+            this.BindRemoteConfig();
+            if (string.IsNullOrEmpty(apiKey)) return;
+            tenjin = global::Tenjin.getInstance(apiKey);
+            Connect();
+        }
+
+        public void Connect()
+        {
+            if (baseEnabled) tenjin?.Connect();
+        }
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            if (!pauseStatus) Connect();
+        }
+
+        public void Transaction(string productId, string currencyCode, int quantity, double unitPrice, string transactionId,
+            string receipt, string signature)
+        {
+            if (!baseEnabled || !iapEnabled) return;
+            tenjin?.Transaction(productId,currencyCode,quantity,unitPrice,transactionId,receipt,signature);
+        }
+    }
+}
